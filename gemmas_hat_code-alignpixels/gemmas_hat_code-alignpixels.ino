a91a84f494f2b14bd27683cd085cf872cbff53ec
inkku <iekman@MacBook-Pro.local>
2016-04-06 19:39:01 +0200

    Fixade en mängd buggar.

--- gemmas_hat_code-alignpixels/gemmas_hat_code-alignpixels.ino
+++ gemmas_hat_code-alignpixels/gemmas_hat_code-alignpixels.ino
@@ -5,17 +5,19 @@
 #define PIN1 1
 #define PIN2 2
 
-char off = 0xff0000; //off
-
-char blue = 0x0000ff; //blue
-char orange = 0xff9900; //orange
-char green = 0x00ff00; //green
+uint32_t off = 0; //off
+uint32_t blue = 1; //blue
+uint32_t orange = 2; //orange
+uint32_t green = 3; //green
 
 //DEBUG VERSION
 static char round1[16] = {blue, off, off, off, off, off, off, off, off, off, off, off, off, off, off, orange}; //first circle
 static char round2[16] = {orange, off, off, off, off, off, off, off, off, off, off, off, off, off, off, green}; //upper part of second circle
 static char round3[12] = {green, off, off, off, off, off, off, off, off, off, off, orange}; //third circle
 static char round4[8] = {orange, off, off, off, off, off, off, blue}; //lower part of second circle
+
+//off, blue, orange, green, orange, green
+uint32_t myColors[4] = {0x000000, 0x000066, 0xff6600, 0x00ff00};
 
 void lightup();
 
@@ -26,7 +28,7 @@ void lightup();
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(52, PIN2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(52, PIN0, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -47,29 +49,30 @@ void setup() {
 void loop() {
 
   void lightup();
+  delay(1000);
 }
 
 
 void lightup() {
   
   for (uint8_t i=0; i<16; i++) {
-    strip.setPixelColor(15-i, round1[i]);
+    strip.setPixelColor(15-i, myColors[round1[i]]);
   }
 
   for(uint8_t i=0; i<16; i++) {
-    strip.setPixelColor(i+20, round2[i]);
+    strip.setPixelColor(i+20, myColors[round2[i]]);
   }
 
   for(uint8_t i=0; i<12; i++) {
-    strip.setPixelColor(i+40, round3[i]);
+    strip.setPixelColor(i+40, myColors[round3[i]]);
   }
 
   for(uint8_t i=0; i<4; i++) {
-    strip.setPixelColor(i+36, round4[i]);
+    strip.setPixelColor(i+36, myColors[round4[i]]);
   }
 
   for(uint8_t i=0; i<4; i++) {
-    strip.setPixelColor(i+16, round4[i+4]);
+    strip.setPixelColor(i+16, myColors[round4[i+4]]);
   }
 
   strip.show();
